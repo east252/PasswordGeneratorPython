58badd993de5f4e41f46fc839628e65ed9d8d77c
Njinir <69115849+east252@users.noreply.github.com>
2025-12-27 15:55:23 -0600

    change from 12 to 16 digits

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -7,21 +7,21 @@ def newPassword ():
         numbers = "1234567890"
         symbols = "!@#$%^&*()"
 
-        # Select 3 random characters from each group
-        a = random.choices(lowers, k=3)
-        lowers2 = a[0]+a[1]+a[2]
-        b = random.choices(uppers, k=3)
-        uppers2 = b[0]+b[1]+b[2]
-        c = random.choices(numbers, k=3)
-        numbers2 = c[0]+c[1]+c[2]
-        d = random.choices(symbols, k=3)
-        symbols2 = d[0]+d[1]+d[2]
+        # Select 4 random characters from each group
+        a = random.choices(lowers, k=4)
+        lowers2 = a[0]+a[1]+a[2]+a[3]
+        b = random.choices(uppers, k=4)
+        uppers2 = b[0]+b[1]+b[2]+b[3]
+        c = random.choices(numbers, k=4)
+        numbers2 = c[0]+c[1]+c[2]+c[3]
+        d = random.choices(symbols, k=4)
+        symbols2 = d[0]+d[1]+d[2]+d[3]
 
-        # Combine the groups into a string 3 lower, 3 upper, 3 number, 3 symbols)
+        # Combine the groups into a string (4 lower, 4 upper, 4 number, 4 symbols)
         combined = lowers2 + uppers2 + numbers2 + symbols2
 
-        # Shuffle the characters in the length of 12 characters.
-        shuffled = random.sample(combined, k=12)
+        # Shuffle the characters in the length of 16 characters.
+        shuffled = random.sample(combined, k=16)
 
         # Join the list of responses into 1 string.
         shuffleCombine = ''.join(shuffled)
